7e0f00a1d01f2f147550f919d089c7576bdbd818
razzzenya <buckingkiska@gmail.com>
2024-02-14 17:34:41 +0400

    Updated source C++ files

---
+++ PP_LAB_1/PP_LAB_1/Source.cpp
@@ -0,0 +1,240 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <time.h>
+#include <chrono>
+
+#define PATH_TO_FIRST_MATRIX "C:\\vscode_repos\\PP_LAB_1\\first matrix.txt"
+#define PATH_TO_SECOND_MATRIX "C:\\vscode_repos\\PP_LAB_1\\second matrix.txt"
+#define PATH_TO_SIZE_FILE "C:\\vscode_repos\\PP_LAB_1\\size.txt"
+#define PATH_TO_RESULT_MATRIX_FILE "C:\\vscode_repos\\PP_LAB_1\\result matrix.txt"
+#define PATH_TO_RESULT_FILE "C:\\vscode_repos\\PP_LAB_1\\result.txt"
+
+class MainProgram {
+public:
+    size_t size = 0;
+    int** first_matrix = nullptr;
+    int** second_matrix = nullptr;
+
+    void LoadSize() {
+        std::ifstream file(PATH_TO_SIZE_FILE);
+
+        if (!file.is_open()) {
+            std::cerr << "File opening error!" << std::endl;
+            return;
+        }
+
+        if (file >> size){
+            file.close();
+            return;
+        }
+
+        else {
+            std::cerr << "Error reading number from file!" << std::endl;
+            file.close();
+        }
+    }
+
+    void AllocateMemoryForMatrices() {
+        if (first_matrix != nullptr && second_matrix != nullptr) {
+            for (size_t i = 0; i < size; ++i) {
+                delete[] first_matrix[i];
+                delete[] second_matrix[i];
+            }
+            delete[] first_matrix;
+            delete[] second_matrix;
+        }
+
+        first_matrix = new int* [size];
+        second_matrix = new int* [size];
+        for (size_t i = 0; i < size; ++i) {
+            first_matrix[i] = new int[size];
+            second_matrix[i] = new int[size];
+        }
+    }
+
+    void GenerateMatrices() {
+        if (!size) {
+            std::cerr << "Matrix size not specified!" << std::endl;
+            return;
+        }
+
+        AllocateMemoryForMatrices();
+
+        srand(time(NULL));
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                first_matrix[i][j] = rand() % 100;
+                second_matrix[i][j] = rand() % 100;
+            }
+        }
+        std::cout << size << " by " << size << " matrices were successfully generated" << std::endl;
+    }
+
+    void SaveMatrices() {
+        std::ofstream file(PATH_TO_FIRST_MATRIX);
+
+        if (!file.is_open()) {
+            std::cerr << "Error opening file for writing!" << std::endl;
+            return;
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                file << first_matrix[i][j] << " ";
+            }
+            file << std::endl;
+        }
+        file.close();
+
+        std::ofstream file2(PATH_TO_SECOND_MATRIX);
+
+        if (!file2.is_open()) {
+            std::cerr << "Error opening file for writing!" << std::endl;
+            return;
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                file2 << second_matrix[i][j] << " ";
+            }
+            file2 << std::endl;
+        }
+        file2.close();
+    }
+
+    void LoadMatrices() {
+        std::ifstream file(PATH_TO_FIRST_MATRIX);
+
+        if (!file.is_open()) {
+            std::cerr << "Error opening first matrix file for reading!" << std::endl;
+            return;
+        }
+
+        AllocateMemoryForMatrices();
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                file >> first_matrix[i][j];
+            }
+        }
+
+        file.close();
+
+        std::ifstream file1(PATH_TO_SECOND_MATRIX);
+
+        if (!file1.is_open()) {
+            std::cerr << "Error opening second matrix file for reading!" << std::endl;
+            return;
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                file1 >> second_matrix[i][j];
+            }
+        }
+    }
+
+    void MultiplyMatricesAndSaveResult() {
+
+        int** result_matrix = new int*[size];
+        for (size_t i = 0; i < size; ++i) {
+            result_matrix[i] = new int[size];
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                result_matrix[i][j] = 0;
+            }
+        }
+
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                for (size_t k = 0; k < size; ++k) {
+                    result_matrix[i][j] += first_matrix[i][k] * second_matrix[k][j];
+                }
+            }
+        }
+        
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+        std::cout << "Time spent in seconds to multiply " << size << " by " << size << " matrices " << elapsed_seconds.count() << std::endl;
+        
+        std::ofstream file(PATH_TO_RESULT_MATRIX_FILE);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file for writing!" << std::endl;
+            return;
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                file << result_matrix[i][j] << " ";
+            }
+            file << std::endl;
+        }
+
+        file.close();
+
+        std::ofstream file1(PATH_TO_RESULT_FILE);
+        file1 << elapsed_seconds.count() << std::endl;
+        file1.close();
+
+        SaveMatrices();
+
+        for (size_t i = 0; i < size; ++i) {
+            delete[] result_matrix[i];
+        }
+        delete[] result_matrix;
+    }
+
+    void PrintMatrices() { //этот метод только для проверки
+        std::cout << "First matrix:" << std::endl;
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                std::cout << first_matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+
+        std::cout << "Second matrix:" << std::endl;
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                std::cout << second_matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    ~MainProgram() {
+        if (first_matrix != nullptr) {
+            for (size_t i = 0; i < size; ++i) {
+                delete[] first_matrix[i];
+            }
+            delete[] first_matrix;
+        }
+
+        if (second_matrix != nullptr) {
+            for (size_t i = 0; i < size; ++i) {
+                delete[] second_matrix[i];
+            }
+            delete[] second_matrix;
+        }
+    }
+
+};
+
+int main() {
+    MainProgram example;
+    example.LoadSize();
+    example.GenerateMatrices();
+    example.MultiplyMatricesAndSaveResult();
+    return 0;
+}
--- check.py
+++ check.py
@@ -1,11 +1,20 @@
 import os
-import numpy
+
+import numpy as np
+
 RESULT_FILENAME = "result.txt"
-FIRST_MATRIX_FILENAME = "first_matrix.txt"
-SECOND_MATRIX_FILENAME = "second_matrix.txt"
+RESULT_MATRIX_FILENAME = "result matrix.txt"
+FIRST_MATRIX_FILENAME = "first matrix.txt"
+SECOND_MATRIX_FILENAME = "second matrix.txt"
 SIZE_FILENAME = "size.txt"
 
 
 if __name__ == "__main__":
     with open(SIZE_FILENAME, "r") as size_file:
         size = int(size_file.read())
+
+    first_matrix = np.loadtxt(FIRST_MATRIX_FILENAME, int)
+    second_matrix = np.loadtxt(SECOND_MATRIX_FILENAME, int)
+
+    loaded_result_matrix = np.loadtxt(RESULT_MATRIX_FILENAME, int)
+    print(loaded_result_matrix)
